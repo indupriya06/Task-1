83449e0ae3b4a22f0afeac51f5486774b6b163f1
indupriya06 <166099784+indupriya06@users.noreply.github.com>
2024-04-05 11:35:01 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,24 +1,22 @@
-# Task-1
-fruits = ["apple", "banana", "orange"] # Create a list of fruits
-fruits.append("grape") # Add a new fruit to the list
-# Print the modified list
-print("List:", fruits)
-
-# Create a dictionary of student information
-student = {
-    "name": "Alice",
-    "age": 18,
-    "grade": "A"
-}
-student["grade"] = "B+" # Modify a value in the dictionary
-# Print the updated dictionary
-print("Dictionary:", student)
-
-# Create a set of numbers
-numbers = {1, 2, 3, 2}  # Sets automatically remove duplicates
-numbers.add(2) # Try adding a duplicate element (won't be reflected)
-# Print the set (duplicates removed)
-print("Set:", numbers)
-numbers.remove(1) # Remove an element from the set
-# Print the modified set
-print("Set after removal:", numbers)
+list=[1,2,3,"hello",4]
+print(list)
+print(list.insert(0,5))
+print(list)
+print(list.remove(3))
+print(list*2)
+dict={1:"one",2:"two",3:"three"}
+print(dict)
+dict2={5:"five"}
+print(dict.update(dict2))
+print(dict.pop(3))
+print(dict)
+dict[4]="four"
+print(dict)
+set={1,2,3,4}
+print(set)
+set.update(("afternoon","good"))
+print(set)
+set.remove("good")
+print(set)
+set.add(10)
+print(set)
